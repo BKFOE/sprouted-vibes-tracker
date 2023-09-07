a3a22233dc873486b7bd2975c73e748edeefc669
bkfoe <tirrell.barronette@gmail.com>
2023-09-07 17:37:16 -0400

    Added backend files to add credentials and secrets

--- send_email.py
+++ send_email.py
@@ -8,8 +8,8 @@ def send_email(message):
     port = 465
     st.write(os.environ["db_username"] == st.secrets["db_username"],
              os.environ["db_password"] == st.secrets["db_password"])
-    # st.write("DB username:", st.secrets["db_username"])
-    # st.write("DB password:", st.secrets["db_password"])
+    st.write("DB username:", st.secrets["db_username"])
+    st.write("DB password:", st.secrets["db_password"])
     username = "tirrell@usprouted.com"
     password = os.getenv("PASSWORD")
     receiver = "tirrell@usprouted.com"
